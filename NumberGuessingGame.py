703b3d32dc7d4b151de10a0063ec96fb686c2095
Iliya-Yadegari <naruto3@email.com>
2021-04-14 19:37:30 +0100

    Update NumberGuessingGame.py

--- NumberGuessingGame.py
+++ NumberGuessingGame.py
@@ -10,13 +10,13 @@ window.title('Number guessing game')
 
 main_frm = LabelFrame(window).grid(row = 0, column = 0, padx = 10, pady = 10)
 
-count = 0
+
 
 lower_lbl = Label(main_frm,text = 'Enter your lower bound ===>').grid(row = 0, column = 0, padx = 10, pady = 10)
 lower_ent = Entry(main_frm)
 upper_lbl = Label(main_frm,text = 'Enter your upper bound ===>').grid(row = 1, column = 0, padx = 10, pady = 10)
 upper_ent = Entry(main_frm)
-submit_btn = Button(window,text = 'Submit',width = 20,height = 3,command = ngm.main_fun).grid(row = 2, column = 0, padx = 10, pady = 10)
+submit_btn = Button(window,text = 'Submit',width = 20,height = 3,command = lambda: ngm.main_fun(lower_ent.get(),upper_ent.get(),window)).grid(row = 2, column = 0, padx = 10, pady = 10)
 
 lower_ent.grid(row = 0, column = 1, padx = 10, pady = 10)
 upper_ent.grid(row = 1, column = 1, padx = 10, pady = 10)
